2f00946eee88e558e1dd80a1bb2d3ddef8a4a427
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-17 19:23:19 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ListRender from "./ListRender";
 
-const Next7Days = (props) => {
+const Next7Days = ({ list }) => {
   const date = new Date();
-
 
   return (
     <div id="next-list">
-      <ListRender list={}/>
+      <ListRender list={list} />
     </div>
   );
 };
-
 export default Next7Days;
