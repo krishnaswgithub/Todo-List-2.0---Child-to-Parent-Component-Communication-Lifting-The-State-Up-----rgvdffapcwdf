216fd52d3ee9e43f06012f9422d59cdeeed793d8
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-17 19:21:20 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,41 +1,78 @@
 import React, { useState, useRef } from "react";
 
-const Inbox = (props) => {
- 
+var curr = new Date();
+
+curr.setDate(curr.getDate());
+
+var date = curr.toISOString().substring(0, 10);
+const object = { title: "", date: date };
+const Inbox = ({ list, append }) => {
+  const [newTask, setNewTask] = useState(false);
+  const [newTodo, setNewTodo] = useState(object);
+  function addNewTask() {
+    setNewTask(true);
+  }
+  function cancelTask() {
+    setNewTask(false);
+    setNewTodo(object);
+  }
+  function handleChange(event) {
+    let id = event.target.id;
+    let value = event.target.value;
+
+    setNewTodo((obj) => ({ ...obj, [id]: value }));
+    return;
+  }
+  function handleClick(event) {
+    event.preventDefault();
+    const { title, date } = newTodo;
+    // if (!title || !date) return;
+    let currentDate = new Date(date);
+    append({ title, date: currentDate });
+    setNewTask(false);
+    setNewTodo(object);
+  }
 
   return (
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={} id='add-new'>
+        <button className="new" onClick={addNewTask} id="add-new">
           +Add New
         </button>
       )}
       {newTask && (
         <form className="newtask-box">
-          <input type="text" id="title" ref={}></input>
+          <input
+            type="text"
+            id="title"
+            value={newTodo.title}
+            onChange={handleChange}
+          ></input>
           <div className="buttons">
-            <button className="new" id="add-list" onClick={}>
+            <button className="new" id="add-list" onClick={handleClick}>
               Add Task
             </button>
-            <button className="new" onClick={}>
+            <button className="new" onClick={cancelTask}>
               Cancel
             </button>
             <input
               type="date"
-              ref={}
-              defaultValue="2022-09-27"
+              onChange={handleChange}
+              defaultValue={date}
               id="date"
             ></input>
           </div>
         </form>
       )}
       <div id="inbox">
-        {props.list.map((list) => {
+        {list.map((list, index) => {
           return (
-            <div className="box" key={}>
+            <div className="box" key={index}>
               <div className="task">
-                {} ({})
+                <div id="code">
+                  {list.title} ({list.date.toLocaleDateString()})
+                </div>
               </div>
             </div>
           );
