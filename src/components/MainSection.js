d41e51d87e7df9a175c8dec46ce440dea13f523c
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-28 14:41:13 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,45 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Inbox from "./Inbox";
 import Next7Days from "./Next7Days";
 import Today from "./Today";
 
 const list = [
-  { number: 1, title: "Let's complete this", date: new Date("6/12/2023") },
-  { number: 2, title: "Should sleep at 9pm", date: new Date("5/28/2023") },
-  { number: 3, title: "Should complete react", date: new Date("10/5/2023") },
+  { number: 1, title: "Let's complete this", date: (new Date()).toLocaleDateString("en-US") },
+  { number: 2, title: "Should sleep at 9pm", date: (new Date()).toLocaleDateString("en-US") },
+  { number: 3, title: "Should complete react", date: (new Date()).toLocaleDateString("en-US") },
 ];
+  
 
-const MainSection = ({ active }) => {
-  const [originalList, setOriginalList] = useState(list);
-  function addTodo({ title, date }) {
-    console.log(title, date);
-    setOriginalList((oldList) => [
-      ...oldList,
-      { number: oldList.length, title, date },
-    ]);
+const MainSection = (props) => {
+  const [mainlist, setli] = useState(list);
+  const [aya, setaya] = useState(0);
+  function appendList(nayaList){
+    
+    setli([...mainlist, nayaList]);
   }
-  function isInWeek(date) {
-    return (
-      date.getDate() - new Date().getDate() >= 0 &&
-      date.getDate() - new Date().getDate() <= 7 &&
-      date.getMonth() == new Date().getMonth() &&
-      date.getYear() == new Date().getYear()
-    );
-  }
-  function isToday(date) {
-    return (
-      date.getDate() == new Date().getDate() &&
-      date.getMonth() == new Date().getMonth() &&
-      date.getYear() == new Date().getYear()
-    );
-  }
-  const todayList = originalList.filter((item) => isToday(item.date));
-  const weekLIst = originalList.filter((item) => isInWeek(item.date));
+  
   return (
     <div className="main-section">
-      {active === "INBOX" && <Inbox list={originalList} append={addTodo} />}
-      {active === "TODAY" && <Today list={todayList} />}
-      {active === "NEXT" && <Next7Days list={weekLIst} />}
+      {props.active === "INBOX" && (
+        <Inbox list={mainlist} append={appendList}/>                                         
+      )}
+      {props.active === "TODAY" && <Today list={mainlist} />}
+      {props.active === "NEXT" && <Next7Days list={mainlist} />}
     </div>
   );
 };
